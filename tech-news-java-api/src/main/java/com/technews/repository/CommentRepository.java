326bef49d4ef4c2b0becbf71d5a184ee137f03d1
Captain <71038018+karimi65@users.noreply.github.com>
2021-09-12 17:45:02 -0500

    fix issues for repository

--- tech-news-java-api/src/main/java/com/technews/repository/CommentRepository.java
+++ tech-news-java-api/src/main/java/com/technews/repository/CommentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@SuppressWarnings("ALL")
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
