4c5490906ba84cbcf3d50745fdc90008f6d31ea8
Captain <71038018+karimi65@users.noreply.github.com>
2021-09-12 11:48:19 -0500

    Create Entity Models

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,21 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+
+        int a = 4;
+        double b = 3.5;
+
+        // calculated variable sum is a double
+        double sum = a + b;
+
+        String c = "cat";
+
+        // calculated variable concat is a String
+        String concat = a + c;
+
+        System.out.println(sum);    // 7.5
+        System.out.println(concat);   // "4cat"
+
+    }
+}
